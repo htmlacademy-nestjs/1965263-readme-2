844fbaf665f1ba9a007c3597409a6663c4ff27dd
Mikhail-Sakharov <94370679+Mikhail-Sakharov@users.noreply.github.com>
2022-11-28 15:40:59 +0300

    Подключил модуль настройки конфигурации соединения с MongoDB

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import databaseConfig from '../config/database.config';
+import {ENV_FILE_PATH} from './app.constant';
 import {AuthModule} from './auth/auth.module';
 import {UserModule} from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig]
+    }),
+    AuthModule,
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
