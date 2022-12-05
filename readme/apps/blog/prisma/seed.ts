3d948c8815482f959ba1483aced6dbbf9c86164e
Mikhail-Sakharov <94370679+Mikhail-Sakharov@users.noreply.github.com>
2022-12-05 16:04:56 +0300

    Добавил функцию для заполнения БД

---
+++ readme/apps/blog/prisma/seed.ts
@@ -0,0 +1,51 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function fillDb() {
+  await prisma.post.upsert({
+    where: {id: 1},
+    update: {},
+    create: {
+      type: 'video',
+      isPublished: true,
+      likes: [
+        '3afa868f-e0d7-450d-bef5-101667e6b836',
+        '3afa868f-e0d7-450d-bef5-101667e6b835',
+        '3afa868f-e0d7-450d-bef5-101667e6b834'
+      ],
+      comments: [
+        {
+          text: 'Comment text 1',
+          userId: '3afa868f-e0d7-450d-bef5-101667e6b835'
+        },
+        {
+          text: 'Comment text 2',
+          userId: '3afa868f-e0d7-450d-bef5-101667e6b834'
+        }
+      ],
+      tags: ['qwe', 'asd', 'zxc'],
+      isRepost: false,
+      authorId: '3afa868f-e0d7-450d-bef5-101667e6b831',
+      originalAuthorId: '3afa868f-e0d7-450d-bef5-101667e6b831',
+      originalId: 1,
+      content: {
+        title: 'Chill Music Lab',
+        url: 'https://www.youtube.com/watch?v=Q7t4JOt-KH4'
+      }
+    }
+  });
+
+  console.info('🤘️ Database was filled')
+}
+
+fillDb()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect()
+
+    process.exit(1);
+  })
